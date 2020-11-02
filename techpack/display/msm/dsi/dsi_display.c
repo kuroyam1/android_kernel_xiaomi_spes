4b35f4e6de1ac1f80f1c7db096e1fc716718bf3d
Danny Lin <danny@kdrag0n.dev>
2020-11-01 16:58:55 -0800

    Suppress overly verbose log spam
    It's hard to see anything that's actually useful with so much verbose
    spam in the log buffer.
    
    Change-Id: If3d0c971df79c7f976e43ed394419d93b7190b43
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -7063,9 +7063,9 @@ int dsi_display_set_mode(struct dsi_display *display,
 			(adj_mode.timing.refresh_rate == 90))
 		dsi_set_backlight_control(display->panel, &adj_mode);
 
-	DSI_INFO("mdp_transfer_time_us=%d us\n",
+	DSI_DEBUG("mdp_transfer_time_us=%d us\n",
 			adj_mode.priv_info->mdp_transfer_time_us);
-	DSI_INFO("hactive= %d,vactive= %d,fps=%d\n",
+	DSI_DEBUG("hactive= %d,vactive= %d,fps=%d\n",
 			timing.h_active, timing.v_active,
 			timing.refresh_rate);
 
